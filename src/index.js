b993b2bc860db624fab265f9f5a1b017d9af7780
virajchovatiya <virajchovatiya03@gmail.com>
2025-01-06 23:25:25 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
+import {app} from "./app.js";
 
 const port = process.env.PORT || 8000
 
